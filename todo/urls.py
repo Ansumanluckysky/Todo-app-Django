96c4c28fc902d69f4194235ac392f7008585b563
Ansumanluckysky <trans.luckysky@gmail.com>
2024-05-20 23:36:03 +0530

    todo app edit delete and other features

--- todo/urls.py
+++ todo/urls.py
@@ -2,5 +2,20 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('addTask/',views.addTask,name='addTask')
+    # Add Task
+    path('addTask/',views.addTask,name='addTask'),
+
+    #Mark as Done
+    path('mark_as_done/<int:pk>',views.mark_as_done,name='mark_as_done'),
+
+    # Edit Feature
+
+    path('edit_task/<int:pk>/', views.edit_task, name='edit_task'),
+
+    # Delete Task
+
+    path('delete_task/<int:pk>/', views.delete_task, name='delete_task'),
+
+
+
 ]
